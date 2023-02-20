4f0eac0805e16751b932fd5ba809dc0bd601a4a8
Ivan Ivanov <me@ivanov.pro>
2023-02-20 09:19:14 +0200

    update deps

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const ELASTIC_USER = 'elastic';
-export const ELASTIC_PASSWORD = 'ioqkveODtpF1vaavlgQAv84f';
-export const ELASTIC_HOST = 'https://db77dce3158a4a90a65a0ef4c1c464dd.us-central1.gcp.cloud.es.io';
+export const ELASTIC_USER = '';
+export const ELASTIC_PASSWORD = '';
+export const ELASTIC_HOST = '';
